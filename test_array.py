927514206649873c4ffeaea86d7a26cb61a5ea70
brunokian <brunokian@hotmail.com>
2023-03-24 18:04:20 -0300

    numpy finalizado

--- test_array.py
+++ test_array.py
@@ -48,6 +48,7 @@ anos_de_nascimento = numpy.loadtxt("anos_de_nascimento.txt")
 
 
 
+
 # array de 2 dimensões
 
 teste = numpy.arange(10).reshape(2,5)
@@ -63,6 +64,8 @@ print(teste.ndim)
 # o resultado seria 2, de duas dimensões
 
 
+
+
 # array de 3 dimensões
 
 teste3 = numpy.arange(60).reshape(3,4,5)
@@ -76,3 +79,48 @@ print(teste3.shape)
 
 print(teste3.ndim)
 # resultado é 3
+
+
+
+# realizando filtros no array
+a = numpy.arange(0,20,3)
+
+print(a)
+
+print(a[a > 12])
+print(a[a%2==0])
+
+
+
+
+# podemos tambem realizar filtros em arrays de mais dimensões
+a = numpy.array([[1,2,3,4], [5,6,7,8]])
+
+print(a)
+print(a[a>=3])
+# retorno: [3 4 5 6 7 8] - note que todos os valores que passam do filtro é retornado em uma unica array
+
+print(a[a>=3].ndim)
+# o retorno: 1 - visto que a resposta é uma array de 1 dimensão
+
+
+
+
+# usando o zeros()
+a = numpy.zeros(3)
+a = numpy.zeros((3))
+# é criado uma array de 1 dimensão com 3 colunas de valor 0
+
+a = numpy.zeros((3,2))
+# é criado uma array de 2 dimensões: um array com 3 arrays com 2 valores cada
+
+a = numpy.zeros((3,2,4))
+# é criado uma array de 3 dimensões: um array com 3 arrays com 2 arrays com 4 valores cada
+
+
+
+# concatenação
+a = numpy.array([[4,7,8], [2,3,8]])
+b = numpy.array([[1,2,3], [3,4,3]])
+c = numpy.concatenate((a,b))
+print(c)
